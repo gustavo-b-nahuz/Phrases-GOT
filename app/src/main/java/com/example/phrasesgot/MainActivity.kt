f13668bdebeda09c9328eadb58d190bd2cea6a3a
Gustavo <gustavobrunoronahuz@gmail.com>
2023-10-08 11:43:56 -0300

    final version

--- app/src/main/java/com/example/phrasesgot/MainActivity.kt
+++ app/src/main/java/com/example/phrasesgot/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.example.phrasesgot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import com.example.phrasesgot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding : ActivityMainBinding;
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.botaoGuardar.setOnClickListener(this);
+        binding.botaoGuardar.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         if(v?.id == R.id.botao_guardar){
-            Toast.makeText(applicationContext, "Teste", Toast.LENGTH_LONG).show()
+            val nome = binding.inputNome.text.toString()
+            val intent = Intent(this, PhraseActivity::class.java)
+            intent.putExtra("name", nome)
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/phrasesgot/PhraseActivity.kt
+++ app/src/main/java/com/example/phrasesgot/PhraseActivity.kt
@@ -2,10 +2,66 @@ package com.example.phrasesgot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import com.example.phrasesgot.databinding.ActivityPhraseBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
-class PhraseActivity : AppCompatActivity() {
+class PhraseActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding : ActivityPhraseBinding
+    private val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_phrase)
+        binding = ActivityPhraseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nome = intent.getStringExtra("name")
+        binding.fraseOla.text = getString(R.string.greetings, nome)
+        binding.botaoGerar.setOnClickListener(this)
+        run("https://api.gameofthronesquotes.xyz/v1/random")
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.botao_gerar){
+            run("https://api.gameofthronesquotes.xyz/v1/random")
+        }
+    }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    try {
+                        val responseBody = response.body()?.string()
+                        val responseJson = JSONObject(responseBody.toString())
+                        val sentence = responseJson.getString("sentence")
+
+                        // Update UI with the extracted sentence (runOnUiThread is used to update UI from a background thread)
+                        runOnUiThread {
+                            binding.fraseGerada.text = sentence
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                } else {
+                    // Handle non-successful response
+                    println("Request failed with status code: ${response.code()}")
+                }
+            }
+        })
     }
 }
